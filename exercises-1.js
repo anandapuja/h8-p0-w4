b68cf139e6794f808917e63ad6bd2e9f82ae79aa
anandapuja <anandapujawandra@gmail.com>
2020-02-04 22:49:39 +0700

    masih mengerjakan angka prima

---
+++ exercises-1.js
@@ -0,0 +1,20 @@
+function angkaPrima(angka) {
+   
+        var angkaGanjilNonPrima;
+
+            var calonPrima;
+            switch (angka) {
+                case 0:
+                    angka % 3 !== 0;
+                    calonPrima = true;
+                break;
+            }
+    // }
+  }
+  
+  // TEST CASES
+//   console.log(angkaPrima(3)); // true
+//   console.log(angkaPrima(7)); // true
+//   console.log(angkaPrima(6)); // false
+//   console.log(angkaPrima(23)); // true
+  console.log(angkaPrima(33)); // false
