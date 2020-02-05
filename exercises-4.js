6428f42e8374cb365593b85b433f24b7afe0ad2e
anandapuja <anandapujawandra@gmail.com>
2020-02-06 06:52:44 +0700

    belum berhasil ngepush angka yang sama ke array multidimensi

--- exercises-4.js
+++ exercises-4.js
@@ -4,23 +4,40 @@ console.log('MENCARI MODUS');
 
 function cariModus(arr) {
 
-    var arrHasil = []; // Tampung hasil akhir
-    var indeks = 0;
-    for( i = 0; i <= arr.length-1; i++ ) {
-        for( j = 0; j <= arr.length-1; j++ ) {
-            if( arr[i] === arr[j] ) {
-                arrHasil.push(arr.slice(j,j+1));
+    for( i = 0; i < arr.length; i++ ){
+        var sorting = [];
+        for( j = 0; j < arr.length; j++ ) {
+            if( arr[i] < arr[j] ) {
+                sorting = arr[j]
+                arr[j] = arr[i];
+                arr[i] = sorting;
             }
         }
     }
 
-    console.log(arrHasil);
+    var hasil = []
+    var kosong;
+    for( i = 0; i < arr.length; i++ ) {
+        for( j = 0; j < arr.length; j++ ) {
+            if( arr[i] == arr[j] && i != j ) {
+                hasil.push(arr[i]);
+            }
+            else {
+                kosong = -1;
+            }
+        }
+    }
 
-  }
+
+    console.log(hasil);
+    console.log(kosong)
+
+    }
+
   
   // TEST CASES
-  console.log(cariModus([10, 4, 5, 2, 4])); // 4
-//   console.log(cariModus([5, 10, 10, 6, 5])); // 5
+//   console.log(cariModus([10, 4, 5, 2, 4])); // 4
+  console.log(cariModus([5, 10, 10, 6, 5])); // 5
 //   console.log(cariModus([10, 3, 1, 2, 5])); // -1
 //   console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
 //   console.log(cariModus([7, 7, 7, 7, 7])); // -1
