e51c4e73b633d2af88212a6135e9353280e14106
anandapuja <anandapujawandra@gmail.com>
2020-02-10 20:34:24 +0700

    selesai mencari modus

--- exercises-4.js
+++ exercises-4.js
@@ -1,43 +1,55 @@
 // Logic Challenge - Mencari Modus
 
-console.log('MENCARI MODUS');
-
 function cariModus(arr) {
 
-    for( i = 0; i < arr.length; i++ ){
-        var sorting = [];
-        for( j = 0; j < arr.length; j++ ) {
-            if( arr[i] < arr[j] ) {
-                sorting = arr[j]
-                arr[j] = arr[i];
-                arr[i] = sorting;
-            }
-        }
-    }
-
-    var hasil = [[]]
-    var kosong;
+    // KELOMPOKKAN ANGKA YANG SAMA DAN JUMLAHKAN
+    var result = [];
     for( i = 0; i < arr.length; i++ ) {
-        for( j = 0; j < arr.length; j++ ) {
-            if( arr[i] == arr[j] && i != j ) {
-                arr.split('');
+        var cek = false;
+        for( j = 0; j < result.length; j++ ) {
+            if( arr[i] === result[j][0] ) {
+                result[j][1]++;
+                cek = true;
             }
-            else {
-                kosong = -1;
-            }
+        }
+        if ( !cek ) {
+            result.push([arr[i], 1]);
         }
     }
 
-
-    console.log(hasil);
-    console.log(kosong)
-
+    // KONDISI JIKA TIDAK DITEMUKAN MODUS
+    var hasilHitung = 0;
+        for( k = 0; k < result.length; k++ ) {
+        if ( result[k][1] === 1 ) {
+            hasilHitung += 1;
+        }
     }
+    if ( hasilHitung === result.length ) {
+        return -1;
+    }
+
+    // KONDISI JIKA DITEMUKAN MODUS
+    if( result.length === 1 ) {
+        return -1
+    }
+    else {
+        var hasil;
+        var penampung = 0;
+        for( i = 0; i < result.length; i++ ) {
+            if( result[i][1] > penampung ) {
+                penampung = result[i][1];
+                hasil = result[i][0];
+            }
+        }
+        return hasil
+    }
+
+}
 
   
   // TEST CASES
-//   console.log(cariModus([10, 4, 5, 2, 4])); // 4
+  console.log(cariModus([10, 4, 5, 2, 4])); // 4
   console.log(cariModus([5, 10, 10, 6, 5])); // 5
-//   console.log(cariModus([10, 3, 1, 2, 5])); // -1
-//   console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-//   console.log(cariModus([7, 7, 7, 7, 7])); // -1
+  console.log(cariModus([10, 3, 1, 2, 5])); // -1
+  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+  console.log(cariModus([7, 7, 7, 7, 7])); // -1
